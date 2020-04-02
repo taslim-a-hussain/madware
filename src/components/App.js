2be8503e3ff765cf81d147283acd0f4f539b7a2e
taz-sen <taz-sen@outlook.com>
2020-04-02 13:54:17 +1300

    init

---
+++ src/components/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <div className="App">
+                <div className="directory-menu">
+
+                    <div className="menu-item">
+                        <div className="content">
+                            <h1 className="title">HATS</h1>
+                            <span className="sub-title">SHOP NOW</span>
+                        </div>
+                    </div>
+
+                    <div className="menu-item">
+                        <div className="content">
+                            <h1 className="title">JACKETS</h1>
+                            <span className="sub-title">SHOP NOW</span>
+                        </div>
+                    </div>
+
+                    <div className="menu-item">
+                        <div className="content">
+                            <h1 className="title">SNEAKERS</h1>
+                            <span className="sub-title">SHOP NOW</span>
+                        </div>
+                    </div>
+
+                    <div className="menu-item">
+                        <div className="content">
+                            <h1 className="title">WOMENS</h1>
+                            <span className="sub-title">SHOP NOW</span>
+                        </div>
+                    </div>
+
+                    <div className="menu-item">
+                        <div className="content">
+                            <h1 className="title">MENS</h1>
+                            <span className="sub-title">SHOP NOW</span>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
