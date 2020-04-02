f7af942eb50b4a8febc7024f9e7382f81e557727
taz-sen <taz-sen@outlook.com>
2020-04-02 23:21:11 +1300

    Save

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 import './App.scss';
 import Directory from '../Directory/Directory';
 
+const Topics = () => (
+    <div>
+        <h1>Topics</h1>
+    </div>
+);
+
+const TopicDetail = () => (
+    <div>
+        <h1>Topic Details</h1>
+        <p>Here are the topic details.</p>
+    </div>
+);
+
 const App = () => (
     <div className="App">
-        <Directory />
+        <Switch>
+            <Route exact path="/" component={Directory} />
+            <Route exact path="/topics" component={Topics} />
+            <Route path="/topics/:topicId" component={TopicDetail} />
+        </Switch>
     </div>
 );
 
