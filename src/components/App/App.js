a4e1ead49ba6da05f6f6444456f192759a597d92
taz-sen <taz-sen@outlook.com>
2020-04-02 22:00:17 +1300

    awesome

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+import Directory from '../Directory/Directory';
+
+const App = () => (
+    <div className="App">
+        <Directory />
+    </div>
+);
+
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-
-import './sass/App.scss';
-
+import App from './components/App/App';
+    
 
 ReactDOM.render(<App />, document.getElementById("root"));
