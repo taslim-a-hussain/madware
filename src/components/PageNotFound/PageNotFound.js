cf5e6a49d40453d3ebc813b89a53f57cf76758e9
Taslim A Hussain <taslim@outlook.co.nz>
2020-04-06 22:34:08 +1200

    404

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -3,6 +3,9 @@ import React from 'react';
 const PageNotFound = (props) => (
     <div>
         <h1>Page not found 404 error</h1>
+        <p>
+            Oops!
+        </p>
         <button onClick={() => props.history.push('/')}>Home</button>
     </div>
 );
